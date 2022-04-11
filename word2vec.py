37c12156f8ef90fc84a6f0bed7e0e3c0432e59a0
charlesbueso <37254269+charlesbueso@users.noreply.github.com>
2022-04-10 21:23:23 -0500

    implemented nltk and gensim

--- word2vec.py
+++ word2vec.py
@@ -1,1 +1,22 @@
-import gensim.downloader as api
+from gensim.test.utils import datapath
+from gensim import utils
+import gensim.models
+import nltk
+nltk.download('brown')
+from nltk.corpus import brown
+
+#training model with Brown corpus
+train_set = brown.sents()
+model = gensim.models.Word2Vec(train_set)
+#saving model
+model.save('brown.embedding')
+brownModel = gensim.models.Word2Vec.load('brown.embedding')
+
+#checking for some similarities for report (3 similar, 3 different)
+print(brownModel.wv.similarity('university','school'))
+print(brownModel.wv.similarity('kid','child'))
+print(brownModel.wv.similarity('clock','watch'))
+
+print(brownModel.wv.similarity('apple','color'))
+print(brownModel.wv.similarity('airplane','apple'))
+print(brownModel.wv.similarity('music','water'))
